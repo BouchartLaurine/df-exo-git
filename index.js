c8e0dce97098a37a151b047f844b9e4a087ec05f
Laurine <bouchartlaurine7@gmail.com>
2025-06-04 09:48:43 +0200

    Exo algo js

---
+++ index.js
@@ -0,0 +1,74 @@
+
+
+ /* 
+    function check (pairimpair) {
+      let nbr;
+      nbr= Number(document.getElementById("myInput").value);
+      if (nbr % 2 == 0 ) {
+     alert("Nombre pair");
+      }else {
+      alert("Nombre impair");
+    }
+    }
+
+  */
+
+    // --------------------------Fonction pair/impair ↑ Exo1-----------------------------------------------
+
+    /*
+    function maxDeux (a, b) {
+      if (a > b) {
+        return a;
+      } else {
+        return b;
+      }
+    }
+
+    console.log(maxDeux(5,10)); //Pour afficher le résultat
+    */
+
+// ---------------------------Fonction le plus grand chiffre ↑ Exo2-------------------------------------------
+
+
+ 
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  function check() {
+      let input = document.getElementById("myInput").value;
+      if (input === "") {
+        alert("Veuillez entrer un nombre");
+      } else {
+        let number = Number(input);
+        if (isNaN(number)) {
+          alert("Ce n'est pas un nombre valide");
+        } else {
+          if (number % 2 === 0) {
+            alert("Nombre pair");
+          } else {
+            alert("Nombre impair");
+          }
+        }
+      }
+   } 
+
+
+*/
